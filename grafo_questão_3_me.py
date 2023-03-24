0746a97fd1935dca958de385e46eea102c34ac6a
Rodriggo Marcelino Developer <rodriggo-santos@hotmail.com>
2023-03-24 12:01:57 -0300

    remoção de parametros para construção do grafo

--- grafo_questão_3_me.py
+++ grafo_questão_3_me.py
@@ -1,32 +1,5 @@
 import networkx as nx
 
-def exibir_grafo_q4(grafo):
-    pos = nx.spring_layout(grafo)
-    nx.draw_networkx_nodes(grafo, pos, node_size=700)
-    nx.draw_networkx_labels(grafo, pos, font_size=20, font_family="sans-serif")
-    nx.draw_networkx_edges(grafo, pos, width=6)
-    nx.draw_networkx_edge_labels(grafo, pos, font_size=20, font_family="sans-serif")
-    plt.axis("off")
-    plt.show()
-
-def adiciona_vertices_q4(grafo, vertices):
-    for v in vertices:
-        if v == 'X1':
-            grafo.add_edge("X1","A")
-            grafo.add_edge("X1","D")
-
-        if v == 'X2':
-            grafo.add_edge("X2","D")
-            grafo.add_edge("X2","E")
-
-        if v == 'X3':
-            grafo.add_edge("X3","E")
-            grafo.add_edge("X3","G")
-
-        if v == 'X4':
-            grafo.add_edge("X4","H")
-            grafo.add_edge("X4","J")
-
 # Instanciando o objeto Grafo
 G4 = nx.Graph()
 
@@ -47,6 +20,24 @@ G4.add_edge("G","H")
 G4.add_edge("H","I")
 G4.add_edge("H","J")
 G4.add_edge("I","J")
+
+def adiciona_vertices_q4(grafo, vertices):
+    for v in vertices:
+        if v == 'X1':
+            grafo.add_edge("X1","A")
+            grafo.add_edge("X1","D")
+
+        if v == 'X2':
+            grafo.add_edge("X2","D")
+            grafo.add_edge("X2","E")
+
+        if v == 'X3':
+            grafo.add_edge("X3","E")
+            grafo.add_edge("X3","G")
+
+        if v == 'X4':
+            grafo.add_edge("X4","H")
+            grafo.add_edge("X4","J")
 
 # Adicionando os novos vértices
 adiciona_vertices_q4(G4,['X1','X2','X3','X4'])
